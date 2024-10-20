9584325001317bfef7b59f57bff3e15f98299d52
muneebul <muneebulgulzar@gmail.com>
2024-10-20 14:40:34 +0530

    updated file

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -38,7 +38,7 @@ const index = () => {
               {/* End personal info */}
 
               <div className="col-12 mt-1">
-                <a className="button" href="" download>
+                <a className="button" href="/" download>
                   <span className="button-text">Download Catalog</span>
                   <span className="button-icon fa fa-download"></span>
                 </a>
